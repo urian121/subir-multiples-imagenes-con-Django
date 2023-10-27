333be3a8fefe7da7cac1a6684627cc7511040bd5
URIAN VIERA <urian1213viera@gmail.com>
2023-10-27 18:28:42 -0500

    Update models.py

--- subir_multiples_imgs/models.py
+++ subir_multiples_imgs/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 
 class Galeria(models.Model):
+    # la palabra upload_to se utiliza para especificar la ubicación en la que se almacenarán los archivos cargados a través del campo models.ImageField
     images = models.ImageField(upload_to='images/')
     created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
